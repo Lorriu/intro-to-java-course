ad461ac4081411a1af2fc4e964576349263f2636
LorriU <l.ugen@hotmail.co.uk>
2023-11-09 12:58:49 -0500

    completed the operators exercise inside java-primitives

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -3,29 +3,60 @@ package com.cbfacademy.operators;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int number1 = 12;
+        int number2 = 12;
+
+        System.out.println(number1++);
+        System.out.println(++number2);
+
+
     }
 
     public static double add(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        double addOperator = operand1 + operand2;
+
+        return addOperator;
+
     }
 
     public static double subtract(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        double subtractOperator = operand1 - operand2;
+
+        return subtractOperator;
+
     }
 
     public static double multiply(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        double multiplyOperator = operand1 * operand2;
+
+        return multiplyOperator;
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        boolean equalCheck = (operand1 == operand2);
+
+        return equalCheck;
+
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        boolean lessThanCheck = (operand1 < operand2);
+
+        return lessThanCheck;
     }
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        
+        boolean isMoreCheck = (operand1 > operand2);
+
+        return isMoreCheck;
+
     }
 }
+
