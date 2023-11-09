c3c57460f3219c0ce7b78de84297adb04d5b1893
LorriU <l.ugen@hotmail.co.uk>
2023-11-09 17:36:29 -0500

    arrays practice code, inside java-primitives app

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -10,6 +10,24 @@ public class App {
         System.out.println(number1++);
         System.out.println(++number2);
 
+        // arrays practice 
+
+            int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8};
+    
+            System.out.println(array1[4]);
+            
+            float[] array2 = {12.4f, 5.6f, 6.8f, 7.6f, 8.6f, 6.6f, 11.2f, 14.5f, 15.6f, 16.5f, 17.8f, 18.7f};
+    
+            System.out.println(array2[4]);
+            
+            double[] array3 = {10.6d, 20.6d, 30.6d, 40.6d, 50.6d};
+    
+            System.out.println(array3[4]);
+    
+            boolean[] array4 = {true, false, false, true, true, false};
+    
+            System.out.println(array4[4]);        
+
 
     }
 
