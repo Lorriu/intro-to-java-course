22a6c615856750c717ea30c54ea10469015d31a8
LorriU <l.ugen@hotmail.co.uk>
2023-12-16 01:07:23 -0500

    ReverseChar homework in OOP

--- homework/object-oriented-programming/src/main/java/com/cbfacademy/words/ReverseCharSequence.java
+++ homework/object-oriented-programming/src/main/java/com/cbfacademy/words/ReverseCharSequence.java
@@ -1,23 +1,44 @@
 package com.cbfacademy.words;
 
+import java.lang.*;
+
+
+
 public class ReverseCharSequence implements CharSequence {
+
+    private String sequenceValue;
+
+    public ReverseCharSequence(String sequenceValue){
+
+         this.sequenceValue = sequenceValue;
+    }
 
     @Override
     public int length() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'length'");
+
+        return sequenceValue.length();
+
     }
 
     @Override
     public char charAt(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'charAt'");
+       
+       return sequenceValue.charAt(length() - index - 1);
+
     }
 
     @Override
     public CharSequence subSequence(int start, int end) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'subSequence'");
+      
+        return new ReverseCharSequence(sequenceValue.substring(length() - end, length() - start));
+    }
+    
+    //reverses the string
+    @Override
+    public String toString(){
+
+        return new StringBuilder(sequenceValue).reverse().toString();
+
     }
     
 }
