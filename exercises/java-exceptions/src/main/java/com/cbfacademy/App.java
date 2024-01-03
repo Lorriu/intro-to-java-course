0b6404836a6628787a124827dcb2acd0d0fe8747
LorriU <l.ugen@hotmail.co.uk>
2024-01-03 15:28:41 -0500

    checked java.exceptions example in main app

--- exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
@@ -1,5 +1,9 @@
 package com.cbfacademy;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,27 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+        FileExtension fileExtension = new FileExtension();
+
+        List<String> filenames = Arrays.asList("App.java", "App.txt", null, "App.md");
+
+        try {
+            Map<String, Integer> result = fileExtension.map(filenames);
+
+        
+
+        for (Map.Entry<String, Integer> entry : result.entrySet()) {
+
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+
+        }
+
+        } catch (NullPointerException e) {
+
+            e.printStackTrace();
+        }
     }
 }
+
