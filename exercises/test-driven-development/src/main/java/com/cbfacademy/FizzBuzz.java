4f605ed7551b26d90232156a3ce908cb9e6c8104
LorriU <l.ugen@hotmail.co.uk>
2024-01-03 17:27:54 -0500

    leapYear & fizzbuzz  exercise in test driven

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,38 +1,33 @@
 package com.cbfacademy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //Create a class `FizzBuzz` 
 
 public class FizzBuzz {
 
 //with a `get` method that takes a number and returns a string, but such that:
-//for multiples of 3, it returns `“Fizz”` instead of the number
-//for multiples of 5, it returns `“Buzz”` instead of the number
-//for multiples of 15, it returns `“FizzBuzz”` instead of the number
-//the string representation of the number otherwise
 
-    public List<String> get(){
+    public String get(int number){
 
-    List<String> fizzBuzzList = new ArrayList<>();
-    
-            for (Integer number : numbers) {
-                if (number % 15 == 0) {
-                    fizzBuzzList.add("FizzBuzz");
-                } else if (number % 3 == 0) {
-                    fizzBuzzList.add("Fizz");
-                } else if (number % 5 == 0) {
-                    fizzBuzzList.add("Buzz");
-                } else {
-                    fizzBuzzList.add(Integer.toString(number));
-                }
+            //for multiples of 15, it returns `“FizzBuzz”` instead of the number
+            if (number % 15 == 0) {
+
+                return "FizzBuzz";
+            
+            //for multiples of 3, it returns `“Fizz”` instead of the number
+            } else if (number % 3 == 0) {
+
+                return"Fizz";
+
+            //for multiples of 5, it returns `“Buzz”` instead of the number        
+            } else if (number % 5 == 0) {
+                
+                return"Buzz";
+            
+            //the string representation of the number otherwise
+            } else {
+                    return(String.valueOf(number));
+
             }
-
-            return fizzBuzzList;
-
-        }
-
-
     
+    }
 }
---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
@@ -0,0 +1,35 @@
+package com.cbfacademy;
+
+//Create a class `Year`
+
+public class Year {
+    
+//with a `isLeap` method that checks whether its input integer is a Leap Year or not:
+
+    public Boolean isLeap(int leapYear){
+
+        //years divisible by `400` are leap years
+        if (leapYear % 400 == 0){
+
+            return true;
+        }
+        //years divisible by `100` but not by `400` are not leap years
+        else if (leapYear % 100 == 0 && leapYear % 400 != 0){
+
+            return false;
+        }
+        //years divisible by `4` but not by `100` are leap years
+        else if (leapYear % 4 == 0 && leapYear % 100 != 0){
+
+            return true;
+        }
+
+        //years not divisible by `4` are not leap years
+        else{
+
+            return false;
+        }
+    
+    }
+
+}
